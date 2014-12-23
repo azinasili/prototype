ffd3c39637b69e5b7d599c670b2ee455622fc41d
Azin Asili <azinasili@gmail.com>
2014-12-22 20:03:19 -0500

    hooked up connect, grunt-open, and grunt watch

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+/*!
+ * Prototype Gruntfile
+ * @author Azin Asili
+ */
+
+'use strict';
+
+/**
+ * Livereload and connect variables
+ */
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({
+  port: LIVERELOAD_PORT
+});
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+/**
+ * Grunt module
+ */
+module.exports = function (grunt) {
+
+  /**
+   * Grunt config
+   */
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    /**
+     * Set project info
+     */
+    project: {
+      src  : 'src',
+      app  : 'app'
+    },
+
+    /**
+     * Project banner
+     * Dynamically appended to CSS files
+     * Inherits text from package.json
+     */
+    tag: {
+      banner: '/*!\n' +
+              ' * <%= pkg.name %>\n' +
+              ' * <%= pkg.title %>\n' +
+              ' * <%= pkg.url %>\n' +
+              ' * @author <%= pkg.author %>\n' +
+              ' * @version <%= pkg.version %>\n' +
+              ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+              ' */\n'
+    },
+
+    /**
+     * Connect port/livereload
+     * https://github.com/gruntjs/grunt-contrib-connect
+     * Starts a local webserver and injects
+     * livereload snippet
+     */
+    connect: {
+      options: {
+        port: 9000,
+        hostname: '*'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [lrSnippet, mountFolder(connect, 'demo')];
+          }
+        }
+      }
+    },
+
+    /**
+     * Opens the web server in the browser
+     * https://github.com/jsoverson/grunt-open
+     */
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+
+    /**
+     * Runs tasks against changed watched files
+     * https://github.com/gruntjs/grunt-contrib-watch
+     * Watching development files and run concat/compile tasks
+     * Livereload the browser once complete
+     */
+    watch: {
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: '<%= project.app %>/{,*/}*.html'
+      }
+    }
+
+  });
+
+  /**
+   * Default task
+   * Run `grunt` on the command line
+   */
+  grunt.registerTask('default', [
+    'connect:livereload',
+    'open',
+    'watch'
+  ]);
+
+};
